cdd4dbc85f06fd0aa613c443c46dea06d0aeb12b
Tom-Soucies <tom.soucies@ens-rennes.fr>
2023-11-08 23:00:23 +0000

    Correction in some definitions

--- Mathlib/Data/Bitvec/AsVectors.lean
+++ Mathlib/Data/Bitvec/AsVectors.lean
@@ -4,16 +4,15 @@ import Mathlib.Data.Vector
 
 #check Vector.mapAccumr
 
-#align_import data.Bitvec.core from "leanprover-community/mathlib"@"1126441d6bccf98c81214a0780c73d499f6721fe"
 /-!
   A `Bitvec n` is morally a sequence of `n` bits.
   This file establishes a way to shift to this perspective by proving the equivalence between
   `Bitvec n` and `Vector bool n`.
-  Furthermore, we show how various (arithmetic) operations on Bitvectors relate to a naive
+  Furthermore, we show how various (arithmetic) operations on bitvectors relate to a naive
   bit-by-bit implementation of those operations using the vector perspective.
 -/
 
-namespace Bitvec
+namespace Std.BitVec
 
 variable {n : Nat}
 universe u v
@@ -22,32 +21,38 @@ universe u v
   ## Pseudo constructors
 -/
 
-/-- The empty Bitvector -/
-def nil : Bitvec 0 :=
-  Bitvec.zero 0
+/-- The empty bitvector -/
+def nil : BitVec 0 :=
+  BitVec.zero 0
 
-/-- Prepend a single bit to the front of a Bitvector. The new bit is the most significant bit -/
-def cons (x : Bool) (xs : Bitvec n) : Bitvec (n+1) :=
-  Vector.cons x xs
+/-- Prepend a single bit to the front of a bitvector. The new bit is the most significant bit -/
+def cons (x : Bool) (xs : BitVec n) : BitVec (n+1) :=
+  BitVec.append xs (cond x 1 0)
 
--- /-- Append a single bit to the end of a Bitvector. The new bit is the least significant bit -/
--- def concat (xs : Bitvec n) (x : Bool) : Bitvec (n+1) :=
---   xs.append (Bitvec.fill 1 x)
+/-- Append a single bit to the end of a bitvector. The new bit is the least significant bit -/
+def concat (xs : BitVec n) (x : Bool) : BitVec (n+1) :=
+  xs.append (BitVec.fill 1 x)
 
-/-- There is only one `empty` Bitvector, namely, `nil` -/
+/-- There is only one `empty` bitvector, namely, `nil` -/
 theorem zero_length_eq_nil :
-    ∀ (xs : Bitvec 0), xs = nil := by simp
+    ∀ (xs : BitVec 0), xs = nil := by
+  intro xs
+  have : xs.toNat < 2 ^ 0 := xs.toFin.prop
+  simp only [Nat.pow_zero, Nat.lt_one_iff] at this
+  have : xs.toNat = nil.toNat := this
+  simp only [nil, BitVec.zero, toNat_inj] at this
+  simp only [this]
 
 /-!
   ## Induction principles
 -/
 
-/-- A custom recursion principle for Bitvectors in terms of `nil` and `cons` -/
+/-- A custom recursion principle for bitvectors in terms of `nil` and `cons` -/
 @[elab_as_elim]
-def consRecursion {motive : {n : Nat} → Bitvec n → Sort u}
+def consRecursion {motive : {n : Nat} → BitVec n → Sort u}
     (nil : motive nil)
-    (cons : {n : Nat} → (x : Bool) → (xs : Bitvec n) → motive xs → motive (cons x xs))
-    {n : Nat} (xs : Bitvec n) : motive xs :=
+    (cons : {n : Nat} → (x : Bool) → (xs : BitVec n) → motive xs → motive (cons x xs))
+    {n : Nat} (xs : BitVec n) : motive xs :=
   /-
     This one might be a bit hard to prove.
     For now, the `consRecursion_nil` and `consRecursion_cons` theorems fully specify how
@@ -62,16 +67,16 @@ theorem consRecursion_nil {motive nil cons} :
   sorry
 
 @[simp]
-theorem consRecursion_cons {motive nil cons} {x : Bool} {xs : Bitvec n} :
-    consRecursion (motive:=motive) nil cons (Bitvec.cons x xs)
+theorem consRecursion_cons {motive nil cons} {x : Bool} {xs : BitVec n} :
+    consRecursion (motive:=motive) nil cons (.cons x xs)
     = cons x xs (consRecursion nil cons xs) := by
   sorry
 
 /-
   `consRecursion` is a so-called custom recursion principle, which allows us to pretend that
-  `Bitvec` is almost an inductive type, with `nil` and `cons` as its constructors.
+  `BitVec` is almost an inductive type, with `nil` and `cons` as its constructors.
 
-  For example, in proofs (using tactic mode) about some `xs : Bitvec n`, we can write
+  For example, in proofs (using tactic mode) about some `xs : BitVec n`, we can write
   ```lean
   induction xs using consRecursion
   ```
@@ -80,10 +85,10 @@ theorem consRecursion_cons {motive nil cons} {x : Bool} {xs : Bitvec n} :
 
   This is why recursion principles are also sometimes called induction principles.
   However, they are also useful beyond proofs. A recursion principle can be used to define a
-  structurally recursive function on Bitvectors.
+  structurally recursive function on bitvectors.
   That is, in `let f := consRecursion h_nil h_cons`, the `h_nil` argument gives the return value of
-  `f Bitvec.nil`, while `h_cons` is a function that maps `x`, `xs` and `f xs` to the return value of
-  `f (Bitvec.cons x xs)`
+  `f BitVec.nil`, while `h_cons` is a function that maps `x`, `xs` and `f xs` to the return value of
+  `f (BitVec.cons x xs)`
 -/
 
 
@@ -91,23 +96,62 @@ theorem consRecursion_cons {motive nil cons} {x : Bool} {xs : Bitvec n} :
   ## Equivalence
 -/
 
-/-- Turn a Bitvector into a vector of bools of the same length -/
-def asVector {n : Nat} : Bitvec n → Vector Bool n :=
+/-- Get the head and tail of a BitVec (head being the least significant bit) -/
+def head : BitVec n → Bool :=
+  fun xs => BitVec.getLsb xs 1
+
+def tail : BitVec (n + 1) → BitVec n :=
+  fun xs => BitVec.ofNat n xs.toNat.div2
+
+theorem cons_head_tail_eq {x : BitVec (n + 1)} :
+    x = cons (head x) (tail x) := by
+  simp only [cons, head, tail]
+  sorry
+
+theorem head_tail_eq {xs ys : BitVec (n + 1)} :
+    xs = ys ↔ head xs = head ys ∧ tail xs = tail ys := by
+  apply Iff.intro
+  case mp =>
+    intro h
+    apply And.intro
+    case left =>
+      rw [←h]
+    case right =>
+      rw [←h]
+  case mpr =>
+    intro h
+    have H : head xs = head ys := h.left
+    have T : tail xs = tail ys := h.right
+    have : xs = cons (head ys) (tail ys) := by
+      rw [←T, ←H]
+      exact cons_head_tail_eq
+    rw [this, ←cons_head_tail_eq]
+
+theorem head_cons {x : Bool} {xs : BitVec n} :
+    head (cons x xs) = x := by
+  sorry
+
+theorem tail_cons {x : Bool} {xs : BitVec n} :
+    tail (cons x xs) = xs := by
+  sorry
+
+/-- Turn a bitvector into a vector of bools of the same length -/
+def asVector {n : Nat} : BitVec n → Vector Bool n :=
   fun xs =>
     consRecursion
       (Vector.nil)
       (fun x _ xs => Vector.cons x xs)
       xs
 
-/-- Turn a vector of bools into a Bitvector of the same length -/
-def ofVector {n : Nat} : Vector Bool n → Bitvec n :=
+/-- Turn a vector of bools into a bitvector of the same length -/
+def ofVector {n : Nat} : Vector Bool n → BitVec n :=
   fun x =>
     match n with
     | 0 => nil
     | Nat.succ _ => cons (Vector.head x) (ofVector <| Vector.tail x)
 
 /-- Distribution of vectorEquiv over cons -/
-theorem asVector_cons {x : Bool} {xs : Bitvec n} :
+theorem asVector_cons {x : Bool} {xs : BitVec n} :
     asVector (cons x xs) = Vector.cons x (asVector xs) := by
   simp only [asVector, ofVector, consRecursion_cons]
 
@@ -118,7 +162,7 @@ theorem ofVector_cons {x : Bool} {xs : Vector Bool n} :
   have H1 : Vector.tail (Vector.cons x xs) = xs := rfl
   rw [H0, H1]
 
-def vectorEquiv {n : Nat} : Bitvec n ≃ Vector Bool n where
+def vectorEquiv {n : Nat} : BitVec n ≃ Vector Bool n where
   toFun := asVector
   invFun := ofVector
   left_inv := fun xs => by
@@ -135,26 +179,20 @@ def vectorEquiv {n : Nat} : Bitvec n ≃ Vector Bool n where
 
 variable {m : Nat}
 
-/-- Get the head and tail of a Bitvec (head being the least significant bit) -/
-def head : Bitvec (n+1) → Bool :=
-  fun xs => Vector.head xs.asVector
-
-def tail : Bitvec (n + 1) → Bitvec n :=
-  fun xs => Vector.tail xs.asVector
-
 /-!
   ## Constants
 -/
 
 theorem zero_asVector :
-    (Bitvec.zero m).asVector = Vector.replicate m false := by
+    (BitVec.zero m).asVector = Vector.replicate m false := by
   induction m
   case zero =>
     apply vectorEquiv.symm.injective
     simp
   case succ m ih =>
-    have : asVector (Bitvec.zero (Nat.succ m)) = Vector.cons false (asVector (Bitvec.zero m)) := by
-      have : Bitvec.zero (Nat.succ m) = cons false (Bitvec.zero m) := by rfl
+    have : asVector (BitVec.zero (Nat.succ m)) = Vector.cons false (asVector (BitVec.zero m)) := by
+      have : BitVec.zero (Nat.succ m) = cons false (BitVec.zero m) := by
+        simp [BitVec.cons, BitVec.zero, BitVec.append]
       rw [this]
       simp only [asVector_cons]
     rw [this]
@@ -167,37 +205,41 @@ theorem zero_asVector :
   ## Bitwise
 -/
 
-theorem complement_asVector {x : Bitvec n} :
+theorem complement_cons {x : Bool} {xs : BitVec n} :
+    ~~~cons x xs = cons (!x) (~~~xs) := by
+  induction xs using consRecursion
+  case nil =>
+    simp only [nil, BitVec.zero, BitVec.cons, Complement.complement]
+    sorry
+  case cons b xs ih =>
+    rw [head_tail_eq]
+    apply And.intro
+    case left =>
+      simp only [head_cons]
+      sorry
+    case right =>
+      simp only [tail_cons]
+      sorry
+
+theorem complement_asVector {x : BitVec n} :
     (~~~x) = (ofVector <| Vector.map not x.asVector) := by
   induction x using consRecursion
   case nil =>
     simp only [ofVector, asVector, Vector.eq_nil]
   case cons b x ih =>
-    simp only [ofVector_cons, asVector_cons, Vector.map_cons]
-    have : ~~~cons b x = cons (!b) (~~~x) := by
-      simp only [Complement.complement]
-      simp only [Bitvec.cons]
-      rfl
-    rw [this, ih]
+    simp only [ofVector_cons, asVector_cons, Vector.map_cons, complement_cons]
+    rw [ih]
 
-variable {x y : Bitvec n}
+variable {x y : BitVec n}
 
 theorem and_asVector :
     (x &&& y) = (ofVector <| Vector.map₂ and x.asVector y.asVector) := by
   induction x using consRecursion
   case nil =>
     simp only [ofVector, asVector, Vector.eq_nil]
+    rfl
   case cons b x ih =>
     simp only [asVector_cons]
-    have : y = Bitvec.cons (head y) (tail y) := by
-      sorry
-    rw [this]
-    simp only [asVector_cons, Vector.map₂_cons]
-    have : cons b x &&& cons (head y) (tail y) = cons (b && head y) (x &&& tail y) := by
-      sorry
-    rw [this]
-    have : cons (b && head y) (x &&& tail y) = cons (b && head y) (ofVector <| Vector.map₂ and x.asVector y.asVector) := by
-      rw [ih]
     sorry
 
 theorem or_asVector :
@@ -205,6 +247,9 @@ theorem or_asVector :
   induction x using consRecursion
   case nil =>
     simp only [ofVector, asVector, Vector.eq_nil]
+    have : y = nil := by simp only [zero_length_eq_nil]
+    rw [this]
+    rfl
   case cons b x ih =>
     simp only [consRecursion_cons, asVector_cons]
     sorry
@@ -214,9 +259,21 @@ theorem xor_asVector :
   induction x using consRecursion
   case nil =>
     simp only [ofVector, asVector, Vector.eq_nil]
+    have : y = nil := by simp only [zero_length_eq_nil]
+    rw [this]
+    rfl
   case cons b x ih =>
     simp only [consRecursion_cons, asVector_cons]
     sorry
+
+theorem shiftLeft_asVector :
+    (x <<< m) = (ofVector <| Vector.cons false x.asVector) := by
+  sorry
+
+theorem ushiftRight_asVector :
+    (x >>> m) = (ofVector <| Vector.tail x.asVector) := by
+  sorry
+
 
 /-
   TODO: `shiftLeft`, `shiftRight`, `rotateLeft`, `rotateRight`
@@ -226,6 +283,9 @@ theorem xor_asVector :
   ## Comparisons
 -/
 
+theorem lt_asVectors :
+    x < y = (asVector x < asVector y) := by
+  sorry
 /-
   TODO: `lt`, `le`, `slt`, `sle`, `sgt`, `sge`
 -/
@@ -268,4 +328,4 @@ theorem sub_asVector :
   We could still formulate bitwise implementatoins, but the question is whether this is even useful
 -/
 
-end Bitvec
+end BitVec
